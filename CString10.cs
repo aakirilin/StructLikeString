1d369f51791c6021ea97fe5e57e501e724794b15
Алексей <58234259+aakirilin@users.noreply.github.com>
2025-11-29 14:27:21 +0400

    Update CString10.cs

--- CString10.cs
+++ CString10.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace CStringLib
 {
     public struct CString10
@@ -23,6 +25,7 @@ namespace CStringLib
                     .OrderBy(f => f.Name)
                     .ToArray();
 
+                if (fields.Length < 0) throw new IndexOutOfRangeException();
                 if (fields.Length < i) throw new IndexOutOfRangeException();
 
                 var result = (char)fields[i].GetValue(instance);
@@ -36,12 +39,30 @@ namespace CStringLib
                     .OrderBy(f => f.Name)
                     .ToArray();
 
+                if (fields.Length < 0) throw new IndexOutOfRangeException();
                 if (fields.Length < i) throw new IndexOutOfRangeException();
 
                 object obj = (object)this;
                 fields[i].SetValue(obj, value);
                 this = (CString10)obj;
             }
+        }
+
+        public void SetString(char[] value)
+        {
+            object obj = (object)this;
+            var type = this.GetType();
+            var fields = type.GetFields()
+                .OrderBy(f => f.Name)
+                .ToArray();
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (i < value.Length) fields[i].SetValue(obj, value[i]);
+                else fields[i].SetValue(obj, new char());
+            }
+
+            this = (CString10)obj;
         }
 
         public void SetString(string value)
@@ -69,12 +90,60 @@ namespace CStringLib
                 .OrderBy(f => f.Name)
                 .Select(f => f.GetValue(instance));
 
-            return string.Join("", fields);
+            var result = string.Join("", fields);
+            return result.Trim();
         }
 
         public override string ToString()
         {
             return GetString();
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if(obj is CString10) return this == (CString10)obj;
+
+            var c1 = new CString10();
+            c1.SetString((string)obj);
+            return this == c1;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(CString10 a, CString10 b)
+        {
+            var a1 = (object)a; 
+            var b1 = (object)b;
+            
+            var type = a.GetType();
+            var fields = type.GetFields();
+            var result = fields.All(f => f.GetValue(a1).Equals(f.GetValue(b1)));
+
+            return result;
+        }
+
+        public static bool operator !=(CString10 a, CString10 b)
+        {
+            var a1 = (object)a;
+            var b1 = (object)b;
+
+            var type = a.GetType();
+            var fields = type.GetFields();
+            var result = fields.Any(f => !f.GetValue(a1).Equals(f.GetValue(b1)));
+
+            return result;
+        }
+
+
+        public static CString10 New(string s)
+        {
+            var result = new CString10();
+            result.SetString(s);
+
+            return result;
         }
     }
 }
